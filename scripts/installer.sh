e7c4cf22a65163b2db5c3c6a80b725f1a1758d93
alonikomax <aloniko.zus@gmail.com>
2017-10-25 19:53:16 +0300

    Update installer.sh

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-sudo dnf update vim-minimal -y
-sudo dnf install -y vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s .config/.vimrc .vimrc
+
