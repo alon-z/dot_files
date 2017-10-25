75dae084eed0de921334f6c020af8713f3fe53ae
alonikomax <aloniko.zus@gmail.com>
2017-10-25 20:12:13 +0300

    Update vim_setup.sh

--- scripts/vim_setup.sh
+++ scripts/vim_setup.sh
@@ -6,5 +6,6 @@ ln -s .config/.vimrc .vimrc
 vim +PluginInstall +qall
 sudo dnf install automake gcc gcc-c++ kernel-devel cmake
 sudo dnf install python-devel python3-devel
+sudo dnf install mono-devel
 cd ~/.vim/bundle/YouCompleteMe/
 ./install.py --clang-completer --omnisharp-completer
