bcb74dab2649305dab48ac638c174ada3da70391
alonikomax <aloniko.zus@gmail.com>
2017-10-25 19:12:04 +0300

    Update install_i3.sh

--- scripts/install_i3.sh
+++ scripts/install_i3.sh
@@ -1,5 +1,6 @@
 # To make sure no need for password for sudo later
 sudo users
+sudo dnf install -y libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake
 cd ~
 
 # clone the repository
