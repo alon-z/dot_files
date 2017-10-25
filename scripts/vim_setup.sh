0cd2d29a8174c77e6d94ac25cecc5e4f68f92041
Alon Zusman <aloniko.zus@gmail.com>
2017-10-25 20:33:02 +0300

    Fixed dnf not using -y

--- scripts/vim_setup.sh
+++ scripts/vim_setup.sh
@@ -4,8 +4,8 @@ sudo dnf install -y vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s .config/.vimrc .vimrc
 vim +PluginInstall +qall
-sudo dnf install automake gcc gcc-c++ kernel-devel cmake
-sudo dnf install python-devel python3-devel
-sudo dnf install mono-devel
-cd ~/.vim/bundle/YouCompleteMe/
+sudo dnf install -y automake gcc gcc-c++ kernel-devel cmake
+sudo dnf install -y python-devel python3-devel
+sudo dnf install -y mono-devel
+cd ~/.vim/pluggin/YouCompleteMe/
 ./install.py --clang-completer --omnisharp-completer
