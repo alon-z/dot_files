1b2e5cbd39a69948ca2e174d6a997a920c31ec0b
alonikomax <aloniko.zus@gmail.com>
2017-10-25 20:09:26 +0300

    Update vim_setup.sh

--- scripts/vim_setup.sh
+++ scripts/vim_setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+sudo dnf update vim-minimal -y
+sudo dnf install -y vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s .config/.vimrc .vimrc
 vim +PluginInstall +qall
+sudo dnf install automake gcc gcc-c++ kernel-devel cmake
+sudo dnf install python-devel python3-devel
+cd ~/.vim/bundle/YouCompleteMe/
+./install.py --clang-completer --omnisharp-completer
