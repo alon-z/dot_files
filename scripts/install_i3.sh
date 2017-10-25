e65e3eb1f24d4b47488c383ed335e7e64898f351
alonikomax <aloniko.zus@gmail.com>
2017-10-25 19:11:16 +0300

    Create install_i3.sh

---
+++ scripts/install_i3.sh
@@ -0,0 +1,18 @@
+# To make sure no need for password for sudo later
+sudo users
+cd ~
+
+# clone the repository
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
